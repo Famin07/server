9e6c0411bf5097aca44f4890069c52f208660abf
famin07 <famin8474@gmail.com>
2022-05-28 13:59:22 +0000

    eddiiiiiiitt

--- server.c
+++ server.c
@@ -25,7 +25,7 @@ int main() {
 	memset(&server_addr, '\0', sizeof(server_addr));
 	server_addr.sin_family = AF_INET;
 	server_addr.sin_port = port;
-	server_addr.sin_addr = inet_addr(ip);
+	server_addr.sin_addr = int_addr(ip);
 
 	n = bind(server_sock, (struct sockaddr*)&server_addr, sizeof(server_addr));
 	if(n < 0) { 
